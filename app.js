9b7762e3b573e5ca0093ca4f249953392a6c1ed9
S4ygak <99271722+S4ygak@users.noreply.github.com>
2025-05-22 18:38:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -211,8 +211,7 @@ function moveTask(sourceColumnId, targetColumnId, taskId) {
 document.addEventListener('DOMContentLoaded', () => {
   renderColumns();
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/ToDoList-PWA/sw.js', {
-      scope: '/ToDoList-PWA/'})
+    navigator.serviceWorker.register('sw.js', { scope: './' })
       .then(() => console.log('SW зарегистрирован'))
       .catch(err => console.log('Ошибка SW:', err));
   }
